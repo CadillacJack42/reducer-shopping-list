b7dbf5a8901dee7a6588a4c15ceada79e794debb
James Keicher <jamesdkeicher@gmail.com>
2022-05-11 16:34:50 -0700

    Setup: skeleton app setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+import Header from './views/Header';
+import ShoppingList from './views/ShoppingList';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <ShoppingList />
+    </>
+  );
+}
---
+++ src/views/ShoppingList.test.jsx
@@ -0,0 +1,3 @@
+describe('Should test the app', () => {
+  it('Should test behaviors', async () => {});
+});
