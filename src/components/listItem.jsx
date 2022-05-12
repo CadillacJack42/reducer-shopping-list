c12f5212238fac736ef23983cedfa510dc0e09bc
James Keicher <jamesdkeicher@gmail.com>
2022-05-12 11:00:13 -0700

    feat: edit button changes li to form and updates list item

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -1,7 +1,42 @@
 import { useData } from '../hooks/useData';
+import { useState } from 'react';
 
 export const ListItem = ({ item }) => {
   const { handleEdit, handleDelete } = useData();
+  const [editing, setEditing] = useState(false);
+
+  let display;
+
+  if (editing) {
+    display = (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setEditing(false);
+        }}
+      >
+        <input
+          value={item.item}
+          onChange={(e) => {
+            handleEdit({
+              ...item,
+              item: e.target.value,
+            });
+          }}
+        />
+        <button type="submit">Save</button>
+      </form>
+    );
+  } else {
+    display = (
+      <>
+        <p>{item.item}</p>
+        <button type="button" onClick={() => setEditing(true)}>
+          Edit Item
+        </button>
+      </>
+    );
+  }
   return (
     <>
       <input
@@ -14,7 +49,7 @@ export const ListItem = ({ item }) => {
           });
         }}
       ></input>
-      {item.item}
+      {display}
       <button type="button" onClick={() => handleDelete(item.id)}>
         Delete Item
       </button>
