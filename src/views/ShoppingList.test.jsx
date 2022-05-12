d058c7fba8d9e9ebf6cb300da875b4e7c4b6d695
James Keicher <jamesdkeicher@gmail.com>
2022-05-12 10:01:10 -0700

    test: Should check for welcome message and item count. Test passes

--- src/views/ShoppingList.test.jsx
+++ src/views/ShoppingList.test.jsx
@@ -1,3 +1,20 @@
+import { screen, render, waitfor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
 describe('Should test the app', () => {
-  it('Should test behaviors', async () => {});
+  it('Should test behaviors', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const welcomeMessage = screen.getByRole('heading', { level: 1 });
+    expect(welcomeMessage).toHaveTextContent(
+      /welcome to cadillac jacks reducer shopping list/i
+    );
+    const cartCount = screen.getByRole('heading', { level: 4 });
+    expect(cartCount).toHaveTextContent(/items in cart/i);
+  });
 });
